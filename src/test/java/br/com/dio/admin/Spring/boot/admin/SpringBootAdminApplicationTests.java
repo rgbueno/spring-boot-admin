9365c9de387e60897323113cc419fa4f997e783e
Rodrigo Gomes Bueno <rodrigobueno.ti@gmail.com>
2024-09-05 15:14:59 -0300

    first commit

---
+++ src/test/java/br/com/dio/admin/Spring/boot/admin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.admin.Spring.boot.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
